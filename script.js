4510fc47cec9f61512a07fc5e6ccc896133e000e
sai-sasank09 <95159792+sai-sasank09@users.noreply.github.com>
2024-10-29 19:05:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('location-form').addEventListener('submit', getWeather);
+
+function getWeather(e) {
+  //Write you code logic here
+
+  // Error should be very specific
+  // Error: Failed to fetch weather data,   should always fetch this error in case of any failure otherwise you test cases will get failed.
+  
+
+}
